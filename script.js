2e43e4e75832aa47b1a7fcfd1eb729619ae8c21d
feelinframe123 <chychynzar1@gmail.com>
2025-01-16 23:37:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+**FEEL.INFRAME PHOTOGRAPHY**
+
+**(Foto Outdoor Wisuda, Event Dokumentasi, Holiday)**
+
+**PAKET FOTO BASIC**
+
+**350k/jam**
+
+Benefit yang di dapatkan:
+
+\- 1 jam sesi foto
+
+\- Bebas berfoto selama sesi berlangsung
+
+\- All file photo in Google drive (bisa diakses hingga 8 hari)
+
+\- Tanpa minimal foto edit premium lightroom
+
+\- Sangat diperbolehkan foto bersama keluarga, pasangan, teman selama
+sesi berlangsung
+
+\- Kami akan menyiapkan moodboard buat kalian yang tidak sempat
+menyiapkan moodboard sendiri
+
+**PAKET FOTO PRIORITAS**
+
+**650k/ 2 jam**
+
+Benefit yang di dapatkan:
+
+\- 2 jam sesi foto
+
+\- Bebas berfoto selama sesi berlangsung
+
+\- Bebas memilih 2 lokasi foto (bebas biaya transport)
+
+\- All file photo in Google drive (bisa diakses hingga 10 hari)
+
+\- Tanpa minimal foto edit premium lightroom
+
+\- Sangat diperbolehkan foto bersama keluarga, pasangan, teman selama
+sesi berlangsung
+
+\- Kami akan menyiapkan moodboard buat kalian yang tidak sempat
+menyiapkan moodboard sendiri
+
+**PAKET FOTO LEBIH HEMAT**
+
+Paket Foto ini sangat cocok untuk kalian yang wisuda-nya bareng dan
+ingin ber-foto berdua dalam nuansa wisuda yang ceria
+
+**450k/Jam**
+
+Benefit yang di dapatkan:
+
+\- Maximal 2 orang in frame
+
+\- 1 jam sesi foto
+
+\- Bebas berfoto selama sesi berlangsung (kalian juga tetap mendapatkan
+beberapa sesi foto perorangan)
+
+\- All file photo in Google drive (bisa diakses hingga 8 hari)
+
+\- Tanpa minimal foto edit premium lightroom
+
+\- Kami akan menyiapkan moodboard buat kalian yang tidak sempat
+menyiapkan moodboard sendiri
+
+!! Pemilihan lokasi akan dikenakan biaya transport tambahan sesuai jarak
+lokasi yang ditentukan (Paket Foto Basic dan Paket Foto Lebih Hemat).
+
+!! Segala aksesoris tambahan yang disiapkan vendor tidak dikenakan biaya
+
+kita ada promo bagi kamu yang mengajak 1 teman-mu untuk booking slot,
+kamu akan mendapatkan promo potongan harga sebesar 50ribu rupiah
+
+Lebih lanjut silahkan lakukan Deposit Pembayaran dulu ya guys.
+
+Contact Person:
+
+Instagram: \@feel.inframe
+
+WhatsApp: +62-823-4569-5620
